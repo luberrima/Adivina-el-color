692a3b038f60e413e5badc78902986b16813ecca
Sebas Osuna <sebasosunah@gmail.com>
2024-11-18 12:25:26 +0100

    generar color aleatorio

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 'use strict';
+
+// GENERAR NUM ALEATORIO
 function numeroAleatorio (min, max) {
-    return Math(Math.random() * (max - min +1)) + min;
+    return Math.floor((Math.random() * (max - min +1)) + min);
 }
+console.log(numeroAleatorio(0, 255));
 
-//funcion para color rgb
+// GENERAR COLOR ALEATORIO
+
 function colorAleatorio() {
-    const r = numeroAleatorio (0, 255);
-    const g = numeroAleatorio (0, 255);
-    const b = numeroAleatorio (0, 255);
-    return `rgb (${r}, ${g}, ${b})`;
+    let r = numeroAleatorio(0, 255);
+    let g = numeroAleatorio(0, 255);
+    let b = numeroAleatorio(0, 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+console.log(colorAleatorio());
+const codigoRGB = document.getElementById('codigoRGB');
+codigoRGB.textContent = colorAleatorio();
+
+function randomColors(num) {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(colorAleatorio());
+    }
+    return arr;
+}
+// console.log(randomColors());
+
+
+//generar color correcto
+function nuevoTurno(){
+    const colorCorrecto = colorAleatorio();
+    codigoRGB.textContent = colorCorrecto.toUpperCase(); 
 }
 
-const contenedorRGB = document.getElementById('contenedorRGB');
-contenedorRGB.style.backgroundColor= `rgb(${r}, ${g}, ${b})`;
+//GENERAR VARIACIONES
+//ASOCIAR COLORES A BOTONES
+const botones = document.getElementsByClassName("btn")
+console.log(botones);
+
+
+//CONTADOR
+
